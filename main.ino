2d9a46eebf6ca51f0547fc996adae5858ed40722
Samveen <samveen@samveen.in>
2018-07-04 18:04:22 +0700

    Initial matrix scan code

--- main.ino
+++ main.ino
@@ -8,28 +8,25 @@
 uint32_t GPIO_IN1=GPIO(25);
 uint32_t GPIO_OUT1=GPIO(20);
 
+uint_fast8_t keycount=0;
+uint_fast8_t loopcount=0;
+
 void setup() {
     Serial.begin(115200);
     Serial.println("KeyPress.ino");
     Serial.println("------------");
     Serial.println("setup()");
 
-    pinMode(LED_RED, OUTPUT); // indicator
-    pinMode(GPIO_IN1, INPUT_PULLUP);
-    pinMode(GPIO_OUT1, OUTPUT);
+    pinMode(LED_RED, OUTPUT); // POWER indicator
+
+    init_keymatrix();
+    Serial.println("Finished setup()");
 }
 
-#define KEY_PRESSED_STATE (LOW)
-uint8_t state;
-
 void loop() {
-    Serial.println("loop()");
-    digitalToggle(GPIO_OUT1);
-    state=digitalRead(GPIO_IN1);
-    digitalToggle(GPIO_OUT1);
-    if (state == KEY_PRESSED_STATE) {
+    keycount=scankeys();
+    Serial.print("Keys pressed: "); Serial.println(keycount);
+    if(!(++loopcount%20))
         ledToggle(LED_RED);
-        Serial.println("\nkey pressed");
-    }
-    delay(1000);
+    delay(100);
 }
