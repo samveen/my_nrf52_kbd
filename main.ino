7fc4c4129c4551f49df036de2983089456c40f08
Samveen <samveen@samveen.in>
2019-12-12 19:17:47 +0530

    Add BlueTooth related code

--- main.ino
+++ main.ino
@@ -13,20 +13,65 @@ uint_fast8_t loopcount=0;
 
 void setup() {
     Serial.begin(115200);
-    Serial.println("KeyPress.ino");
+    while ( !Serial ) delay(10);   // for nrf52840 with native usb
+
+    Serial.println("KeyPress");
     Serial.println("------------");
-    Serial.println("setup()");
+    Serial.println("main:setup()");
 
     pinMode(LED_RED, OUTPUT); // POWER indicator
 
+    // Setup BT
+    //setup_bt();
+
+    // Keymatrix initialization
     init_keymatrix();
-    Serial.println("Finished setup()");
+
+    Serial.println("main:setup()::end");
 }
 
 void loop() {
+    if(!(++loopcount%20)) // Loop indicator LED
+        digitalToggle(LED_RED);
+
     keycount=scankeys();
-    Serial.print("Keys pressed: "); Serial.println(keycount);
-    if(!(++loopcount%20))
-        ledToggle(LED_RED);
+
+    if(keycount>0) { // Keypress LED
+        digitalToggle(LED_BLUE);
+        Serial.print("Keys pressed: "); Serial.println(keycount);
+    }
+
     delay(100);
 }
+
+/* // Scan from serial and write to BT code from NRF52 examples 
+void loop()
+{
+  // Only send KeyRelease if previously pressed to avoid sending
+  // multiple keyRelease reports (that consume memory and bandwidth)
+  if ( hasKeyPressed )
+  {
+    hasKeyPressed = false;
+    blehid.keyRelease();
+
+    // Delay a bit after a report
+    delay(5);
+  }
+
+  if (Serial.available())
+  {
+    char ch = (char) Serial.read();
+
+    // echo
+    Serial.write(ch);
+
+    blehid.keyPress(ch);
+    hasKeyPressed = true;
+
+    // Delay a bit after a report
+    delay(5);
+  }
+}
+*/
+
+// vim: nowrap ts=4 sw=4 et:
